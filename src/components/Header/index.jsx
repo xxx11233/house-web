8c8c39067ad57613e0aade12ef0e71f079df30a5
xxx11233 <1255945493@qq.com>
2023-05-07 11:49:54 +0800

    complete

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,58 @@
-import React, { memo, useContext } from 'react'
-import { Layout } from 'antd'
+import React, { memo, useContext, useMemo, useState, useEffect } from 'react'
+import { Layout, Select } from 'antd'
 import styles from './index.module.less'
 import { AppContext } from '../../store'
+import moment from 'moment'
 
+const weatherList = ['晴', '大雨', '小雨', '雪', '雨加雪', '多云']
 function index() {
-  const { dispatch } = useContext(AppContext)
+  const { city, currentCity, dispatch, country, currentCountry } = useContext(AppContext)
+  const cityOptions = useMemo(
+    () => city[currentCountry].map((label, index) => ({ label, value: index })),
+    [currentCountry],
+  )
+  const countryOptions = useMemo(() => country.map((label, index) => ({ label, value: index })), [])
+
+  const weater = useMemo(
+    () => weatherList[parseInt(Math.random() * weatherList.length, 10)],
+    [currentCountry, currentCity],
+  )
+
+  const [time, setTime] = useState('')
+
+  useEffect(() => {
+    const tick = setInterval(() => {
+      setTime(moment(new Date()).format('YYYY-MM-DD hh:mm:ss'))
+    }, 1000)
+    return () => clearInterval(tick)
+  })
 
   return (
     <Layout.Header className={styles.header}>
-      <span
-        onClick={() => {
-          dispatch('addCount')
-        }}
-      >
-        count++
-      </span>
+      <div className={styles.select}>
+        <img src={require('../../assets/images/logo.svg')} alt="" />
+        <Select
+          value={currentCountry}
+          style={{ width: 120, marginRight: 15 }}
+          options={countryOptions}
+          onChange={(value) => dispatch('setCountry', value)}
+        />
+        <Select
+          value={currentCity}
+          style={{
+            width: 120,
+          }}
+          options={cityOptions}
+          onChange={(value) => dispatch('setCity', value)}
+        />
+      </div>
+      <div className={styles.vacation}>
+        <span style={{ marginRight: 15 }}>
+          {country[currentCountry]}全年假期: {country[currentCountry] !== '中国' ? 30 : 15}天
+        </span>
+        {city[currentCountry][currentCity]}天气: {weater}
+      </div>
+      <div className={styles.time}>{time}</div>
     </Layout.Header>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,32 @@
 import React, { useContext } from 'react'
 import styles from './index.module.less'
 import { AppContext } from '../../store'
+import { Card, Space, Tag } from 'antd'
+const colorList = ['magenta', 'red', 'volcano']
 export default function index() {
-  const { count } = useContext(AppContext)
-  return <div className={styles.home}>当前的count：{count}</div>
+  const { data, currentCity, city, currentCountry } = useContext(AppContext)
+  return (
+    <div className={styles.home}>
+      {data[currentCity].map((item, index) => (
+        <Card
+          style={{ width: '32%', marginTop: 30, marginRight: index % 3 === 2 ? 0 : '2%' }}
+          key={city[currentCountry][currentCity] + item.name + index}
+        >
+          <div className={styles['content-bg']} />
+          <h3>{`${city[currentCountry][currentCity]}·${item.name}`}</h3>
+          <div className={styles['content']}>
+            <span>{item.size}</span>
+            <span>{item.price}</span>
+          </div>
+          <Space wrap style={{ paddingBottom: 30, paddingLeft: 15 }}>
+            {item.desc.map((content, index) => (
+              <Tag key={content} color={colorList[index]}>
+                {content}
+              </Tag>
+            ))}
+          </Space>
+        </Card>
+      ))}
+    </div>
+  )
 }
